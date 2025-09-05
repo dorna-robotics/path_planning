904ec9a0ed604f2343fd6a4b9318ac0c64cdefbc
javad <smjty25@gmail.com>
2025-09-06 00:51:18 +0330

    check collision function first submit

---
+++ path_planing/node.py
@@ -0,0 +1,194 @@
+import numpy as np
+import fcl
+import sys
+
+import dorna2.pose as dp
+
+def axis_angle_to_quaternion(axis_angle):
+    theta = np.linalg.norm(axis_angle)
+    if theta < 1e-6:
+        return [0, 0, 0, 1]
+    axis = axis_angle / theta
+    x, y, z = axis
+    theta = theta * 180.0 / np.pi
+    s = np.sin(theta / 2.0)
+    return [x * s, y * s, z * s, np.cos(theta / 2.0)]
+
+
+def transform_to_matrix(xyz, rvec):
+    quat = axis_angle_to_quaternion(rvec)
+    
+    # compatibility across FCL builds
+    if hasattr(fcl, "Transform"):
+        tf = fcl.Transform(quat, xyz)
+    elif hasattr(fcl, "Transform3f"):
+        tf = fcl.Transform3f(quat, xyz)
+    elif hasattr(fcl, "Transformd"):
+        tf = fcl.Transformd(quat, xyz)
+    else:
+        raise ImportError("No suitable Transform class found in fcl module.")
+    
+    return tf, quat
+
+
+class unified_object:
+    def __init__(self, fcl_obj, pybullet_id, mat, fcl_shape):
+        self.fcl_object = fcl_obj
+        self.pybullet_id = pybullet_id
+        self.mat = mat
+        self.fcl_shape = fcl_shape
+
+#some good functions
+
+def fcl_transform_from_matrix(matrix4x4):
+    rotation = matrix4x4[:3, :3]
+    translation = matrix4x4[:3, 3]
+
+    # ensure numpy vector is proper type
+    translation = np.array(translation, dtype=np.float32)
+
+    # try to use rotation matrix directly if supported
+    if hasattr(fcl, "Transform"):
+        return fcl.Transform(rotation, translation)
+    elif hasattr(fcl, "Transform3f"):
+        try:
+            return fcl.Transform3f(rotation, translation)
+        except TypeError:
+            # some builds want quaternion instead of rotation matrix
+            from scipy.spatial.transform import Rotation as R
+            quat = R.from_matrix(rotation).as_quat()  # [x, y, z, w]
+            return fcl.Transform3f(quat, translation)
+    elif hasattr(fcl, "Transformd"):
+        from scipy.spatial.transform import Rotation as R
+        quat = R.from_matrix(rotation).as_quat()
+        return fcl.Transformd(quat, translation)
+    else:
+        raise ImportError("No suitable Transform class found in fcl module.")
+
+def create_cube(xyz_rvec, scale=[1,1,1]):
+    xyz = xyz_rvec[:3]
+    rvec = xyz_rvec[3:]
+    tf, quat = transform_to_matrix(xyz,rvec)
+    mat = dp.xyzabc_to_T(xyz_rvec)
+
+    # FCL
+    half_extents = [s / 2.0 for s in scale]
+    box = fcl.Box(*scale)
+    fcl_obj = fcl.CollisionObject(box, tf)
+
+    # PyBullet
+    body_id = 0
+
+    return unified_object(fcl_obj, body_id, mat, box)
+
+"""
+def create_mesh(mesh_path, xyz_rvec, scale=[1,1,1]):
+    xyz = xyz_rvec[:3]
+    rvec = xyz_rvec[3:]
+    tf, quat = transform_to_matrix(xyz, rvec)
+    mat = dp.xyzabc_to_T(xyz_rvec)
+
+    # Load mesh
+    mesh = trimesh.load(mesh_path, force='mesh').as_triangles()
+    mesh.apply_scale(scale)
+    
+    vertices = mesh.vertices.tolist()
+    triangles = [list(face) for face in mesh.faces]
+
+    # FCL
+    bvh = fcl.BVHModel()
+    bvh.beginModel(len(vertices), len(triangles))
+    bvh.addSubModel(vertices, triangles)
+    bvh.endModel()
+    fcl_obj = fcl.CollisionObject(bvh, tf)
+
+    # Save mesh as temp .obj for PyBullet
+    with tempfile.NamedTemporaryFile(suffix=".obj", delete=False, mode='w') as f:
+        for v in mesh.vertices:
+            f.write(f"v {v[0]} {v[1]} {v[2]}\n")
+        for tri in mesh.faces:
+            f.write(f"f {tri[0]+1} {tri[1]+1} {tri[2]+1}\n")
+        mesh_file = f.name
+
+    body_id = 0
+
+    os.remove(mesh_file)
+    return unified_object(fcl_obj, body_id, mat, bvh)
+"""
+
+def create_sphere(xyz_rvec, scale=[1,1,1]):
+    xyz = xyz_rvec[:3]
+    rvec = xyz_rvec[3:]
+    tf, quat = transform_to_matrix(xyz, rvec)
+    mat = dp.xyzabc_to_T(xyz_rvec)
+    radius = scale[0]  # uniform scale only
+    sphere = fcl.Sphere(radius)
+    fcl_obj = fcl.CollisionObject(sphere, tf)
+
+    body_id = 0
+
+    return unified_object(fcl_obj, body_id, mat, sphere)
+
+
+class Node:
+    def __init__(self, name, parent=None, local_transform=None, tags=[], target_tags=[]):
+        self.name = name
+        self.parent = None
+        self.children = []
+        self.lock = False
+        self.local_transform = local_transform if local_transform is not None else np.eye(4)
+
+        # Geometry
+        self.collisions = []  # List of collision objects
+
+        self.tags = set(tags)
+        self.target_tags = set(target_tags)
+
+        if parent:
+            self.set_parent(parent)
+
+
+    def get_global_transform(self):
+        if self.parent:
+            return self.parent.get_global_transform() @ self.local_transform
+        return self.local_transform
+
+
+    def set_local_transform(self, transform):
+        self.local_transform = transform
+
+
+    def set_parent(self, new_parent):
+        if self.parent:
+            self.parent.children.remove(self)
+        self.parent = new_parent
+        new_parent.children.append(self)
+
+
+    def move_to(self, new_parent):
+        """Reparents node while keeping global transform unchanged."""
+        if self.lock: #locked nodes such as robot nodes, wont move around in tree
+            print("Simulation Warning: Reparenting locked nodes is not possible!")
+            return
+
+        global_tf = self.get_global_transform()
+        inv_new_parent_global = np.linalg.inv(new_parent.get_global_transform())
+        new_local = inv_new_parent_global @ global_tf
+        self.set_parent(new_parent)
+        self.local_transform = new_local
+
+
+    #add a new collision shape to this node, obj is a fcl collision obj. tf is the 4x4 matrix transformation in
+    #the nodes local coordiante system
+    def add_collision(self, obj, tf): 
+        t1 = fcl_transform_from_matrix(self.get_global_transform() @ tf)
+        collision_obj = fcl.CollisionObject(obj, t1)
+        self.collisions.append(CollisionShape(self,tf, obj, collision_obj))
+
+
+    def update_collision_object_transforms(self, gt=None):
+        if gt is None:
+            gt = self.get_global_transform()
+        for coll_obj in self.collisions:
+            new_tf = fcl_transform_from_matrix(gt @ coll_obj.mat)
+            coll_obj.fcl_object.setTransform(new_tf)
---
+++ path_planing/urdf.py
@@ -0,0 +1,114 @@
+from urchin import URDF
+import numpy as np
+import fcl
+import time
+
+import dorna2.pose as dp
+import node as nd 
+
+class urdf_robot:
+
+    def urdf_joint_transform(self, joint, value=0.0):
+        """Get joint transform at a given joint value (for revolute or prismatic)."""
+        T = np.eye(4)
+        T = joint.origin
+
+        axis = np.array(joint.axis)
+        if joint.joint_type == 'revolute':
+            R_axis = dp.abc_to_rmat(value * axis)
+            motion = np.eye(4)
+            motion[:3, :3] = R_axis
+            return T @ motion
+
+        elif joint.joint_type == 'prismatic':
+            motion = np.eye(4)
+            motion[:3, 3] = value * axis
+            return T @ motion
+
+        else:  # fixed or unsupported
+            return T
+
+    def __init__(self, urdf_file, joint_values={}, parent = None):
+        self.robot = URDF.load(urdf_file)
+        self.all_objs = []
+        self.link_nodes = {}
+        self.link_names = []
+        self.prnt_map = {}
+
+        def recurse(link_name, parent_node):
+            link = self.robot.link_map[link_name]
+            self.link_names.append(link_name)
+            joint = next((j for j in self.robot.joints if j.child == link_name), None)
+            joint_value = joint_values.get(joint.name, 0.0) if joint else 0.0
+
+            local_tf = self.urdf_joint_transform(joint, joint_value) if joint else np.eye(4)
+
+            node = nd.Node(link.name, parent_node, local_tf)
+            node.lock = True
+
+            self.link_nodes[link.name] = node
+
+
+            # Visuals and Collisions
+            #for vis in link.visuals:
+            #    node.visuals.append(vis.geometry)  # optionally convert to your own mesh format
+
+            for col in link.collisions:
+                shape = col.geometry
+                tf = np.eye(4)            
+                if col.origin is not None:
+                    tf = np.matrix(col.origin)
+
+                xyzabc = dp.T_to_xyzabc(tf)
+                # Example: only support box/sphere/cylinder for now
+                if shape.box:
+                    half_extents = shape.box.size
+                    obj = nd.create_cube(xyzabc, scale = half_extents)
+                elif shape.sphere:
+                    obj = nd.create_sphere(xyzabc, scale = [1,1,1])
+
+                else:
+                    obj = None
+                    continue
+
+                self.prnt_map[id(obj.fcl_shape)] = node
+                node.collisions.append(obj)
+                self.all_objs.append(obj)
+
+            for j in self.robot.joints:
+                if j.parent == link_name:
+                    recurse(j.child, node)
+
+        self.root_link = self.robot.base_link
+        self.root_node = nd.Node(self.root_link.name, parent)
+        self.link_nodes[self.root_link.name] = self.root_node
+        recurse(self.root_link.name, self.root_node)
+
+
+    def set_joint_values(self, joints = [0,0,0,0,0,0,0], offset_mat = np.eye(4)):
+
+        def recurse(link_name, parent_node, joints, count ,parent_tf):
+            link = self.robot.link_map[link_name]
+            joint = next((j for j in self.robot.joints if j.child == link_name), None)
+            
+            if count>-1:
+                joint_value = joints[count]# * np.pi / 180.0
+            else:
+                joint_value = 0
+
+
+            local_tf = self.urdf_joint_transform(joint, joint_value) if joint else np.eye(4)
+
+
+            g_tf = parent_tf @ local_tf
+
+            node = parent_node.children[0]
+            node.lock = True
+
+            node.set_local_transform(local_tf)
+            node.update_collision_object_transforms(g_tf)
+
+            for j in self.robot.joints:
+                if j.parent == link_name:
+                    recurse(j.child, node, joints, count + 1, g_tf)
+        recurse(self.root_link.name, self.root_node, joints, -1 , offset_mat)
