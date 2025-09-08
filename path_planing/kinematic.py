5f44b333849dad6fdcff29b8d161a11e42b1edd3
smhty <hussein.ty@gmail.com>
2025-09-08 12:06:23 -0700

    kinematic

---
+++ path_planing/kinematic.py
@@ -0,0 +1,39 @@
+import dorna2.pose as dorna_pose
+from dorna2 import Pose
+import numpy as np
+
+
+class Inverse_kinematic_error(Exception):
+    """Raised when inverse kinematics cannot find a valid solution."""
+    pass
+
+
+class Kinematic():
+    def __init__(self, robot=None, base_in_world=[0, 0, 0, 0, 0, 0]):
+        self.robot = robot
+        self.base_in_world = base_in_world
+
+    def inv(self, pose_in_world, aux=[0, 0], tool=[0, 0, 0, 0, 0], init_joint=None, freedom=None):
+        try:
+            # current joint
+            self.robot.kinematic.set_tcp_xyzabc(tool)
+            current_joint = init_joint if init_joint is not None else self.robot.get_all_joint()
+            current_joint = (current_joint + [0, 0, 0, 0, 0, 0, 0, 0])[:8]
+
+            # target joint
+            pose_in_robot = np.array(dorna_pose.frame_to_robot(pose_in_world,
+                                                            aux=aux, 
+                                                            base_in_world=self.base_in_world)) 
+            _joint = np.append(self.robot.kinematic.inv(pose_in_robot, current_joint[0:6], False, freedom=freedom)[0], aux)
+            
+            # cehck if there is a collision then set the joint
+            joint = _joint.tolist()
+                       
+        except Inverse_kinematic_error:
+            # re-raise so main loop can catch it
+            raise
+        finally:
+            # reset tool
+            self.robot.kinematic.set_tcp_xyzabc([0, 0, 0, 0, 0, 0])
+
+        return joint, {"j"+str(i): float(joint[i]) for i in range(len(joint))}
